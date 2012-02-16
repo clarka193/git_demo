d9fd8e73d32180a964e781cc3f2daebfd55ee8eb
Anthony Clark <clarka193@potsdam.edu>
2012-02-16 18:38:29 -0500

    Added getCount()

--- print.c
+++ print.c
@@ -3,7 +3,12 @@
 // ------~---------~
 
 #include <stdio.h>
+#include <string.h>
 #include "print.h"
+
+int getCount(char* str){
+  return strlen(str);
+}
 
 int main(int argc, char* argv[]){
   name = "Anthony Clark";
