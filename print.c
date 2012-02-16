a24651a2233a12f8e7d7fc119b3cd5987269d18a
Bob Smith <bob@smith.com>
2012-02-16 18:43:12 -0500

    Removed function

--- print.c
+++ print.c
@@ -3,12 +3,7 @@
 // ------~---------~
 
 #include <stdio.h>
-#include <string.h>
 #include "print.h"
-
-int getCount(char* str){
-  return strlen(str);
-}
 
 int main(int argc, char* argv[]){
   name = "Anthony Clark";
